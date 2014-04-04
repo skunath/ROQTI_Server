87edc8c0130e9d4342cbd8e0e3cdbb868f5b995a
S Kunath <skunath@gmail.com>
2014-04-04 02:09:25 -0400

    fixes for lib

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,10 @@ set :deploy_to, '/projects/rails_apps/roqti_server'
 set :pty, true
 set :ssh_options, { :forward_agent => true }
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
